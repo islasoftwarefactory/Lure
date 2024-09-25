c0726c5ae43b9d5d8916d118fa045d1668939445
Migxel <mtormes775@gmail.com>
2024-09-25 08:18:39 -0300

    feat: now project have database migration

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from BackEnd.Database.config import database_uri
-from BackEnd.Database.connection import connect_to_db, init_db
+from BackEnd.Database.connection import connect_to_db, init_db, db
 from sqlalchemy.exc import OperationalError
 from BackEnd.Routes.blueprints import register_blueprints
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 register_blueprints(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri()
+
+migrate = Migrate(app, db)
 init_db(app)
 
 try:
