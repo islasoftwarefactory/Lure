e69c1d37f24658b60a54de83c5e85e191f8019ba
Migxel <mtormes775@gmail.com>
2024-09-30 10:47:19 -0300

    refact: column here have maxim caracteres

--- BackEnd/Database/Models/ImageCategory.py
+++ BackEnd/Database/Models/ImageCategory.py
@@ -6,7 +6,7 @@ class ImageCategory(db.Model):
     __tablename__ = "image_categories"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('America/Sao_Paulo')))
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('America/Sao_Paulo')), onupdate=datetime.now(pytz.timezone('America/Sao_Paulo')))
 
