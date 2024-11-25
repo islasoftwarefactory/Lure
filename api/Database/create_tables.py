b73998f13c19d896fb967efa742d8ab2b16777f1
Migxel <mtormes775@gmail.com>
2024-11-25 16:48:16 -0300

    feat: import image category model

--- api/Database/create_tables.py
+++ api/Database/create_tables.py
@@ -1,9 +1,9 @@
-from .Models import Address, Category, Discount, Gender, Payment, PaymentMethod, Product, Size, User
+from .Models import Address, Category, Discount, Gender, Payment, PaymentMethod, Product, Size, User, ImageCategory
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def create_tables():
-    models = [Address, Category, Discount, Gender, Payment, PaymentMethod, Product, Size, User]
+    models = [Address, Category, Discount, Gender, Payment, PaymentMethod, Product, Size, User, ImageCategory]
     for model in models:
         db.Model.metadata.create_all(bind=db.engine)
