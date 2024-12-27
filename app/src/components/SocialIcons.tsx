8951794be18a2f9f730385eb796358886668622f
Migxel <mtormes775@gmail.com>
2024-12-27 18:59:10 -0300

    feat: update SocialIcons component with new Instagram link and styling improvements
    - Replaced TikTok and Pinterest icons with comments for future use.
    - Updated Instagram link to direct to the official page.
    - Enhanced styling of SocialIconLink for better visibility with a darker background and white text.
    - Improved layout consistency in the SocialIcons component.

--- app/src/components/SocialIcons.tsx
+++ app/src/components/SocialIcons.tsx
@@ -1,17 +1,25 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import { Mail } from 'lucide-react'; // Importando apenas o ícone Mail
-import TikTokIcon from '../assets/icons/home/TikTok.svg';
-import PinterestIcon from '../assets/icons/home/Pinterest.svg';
-import InstagramIcon from '../assets/icons/home/Instagram.svg';
+import { Mail } from 'lucide-react';
+// import { TikTok } from 'lucide-react';
+// import PinterestIcon from '../assets/icons/home/Pinterest.svg';
+import InstagramIcon from '../assets/icons/home/instagram.svg';
 
 export const SocialIcons: React.FC = () => {
   return (
     <div className="p-4 h-[60px] flex justify-end space-x-4">
-      <SocialIconLink to="/contact" icon={Mail} alt="Contact" /> {/* Usando o componente Mail para contato */}
-      <SocialIconLink to="/instagram" icon={InstagramIcon} alt="Instagram" />
-      <SocialIconLink to="/tiktok" icon={TikTokIcon} alt="TikTok" />
-      <SocialIconLink to="/pinterest" icon={PinterestIcon} alt="Pinterest" />
+      <SocialIconLink to="/contact" icon={Mail} alt="Contact" />
+      <SocialIconLink 
+        to="https://www.instagram.com/lure.us/" 
+        icon={InstagramIcon} 
+        alt="Instagram" 
+      />
+      {/* <SocialIconLink 
+        to="/tiktok" 
+        icon={(props) => <TikTok {...props} fill="white" stroke="white" />} 
+        alt="TikTok" 
+      /> */}
+      {/* <SocialIconLink to="/pinterest" icon={PinterestIcon} alt="Pinterest" /> */}
     </div>
   );
 };
@@ -26,9 +34,9 @@ const SocialIconLink: React.FC<SocialIconLinkProps> = ({ to, icon: Icon, alt }) 
   <motion.div
     whileHover={{ scale: 1.1 }}
     whileTap={{ scale: 0.9 }}
-    className="bg-gray-200 rounded-full p-2 transition duration-300 ease-in-out hover:shadow-md"
+    className="bg-[#090707] rounded-full p-2 w-10 h-10 flex items-center justify-center transition duration-300 ease-in-out hover:shadow-md"
   >
-    <Link to={to} className="text-black">
+    <Link to={to} className="text-white">
       {typeof Icon === 'string' ? (
         <img src={Icon} alt={alt} className="w-6 h-6" />
       ) : (
