e327fbad1daa1329449515378835399af8c998e6
Migguell <mtormes775@gmail.com>
2025-06-26 17:50:09 -0300

    feat

--- api/app.py
+++ api/app.py
@@ -62,6 +62,7 @@ allowed_origins = [
     "http://localhost:5173",       # Desenvolvimento (Vite)
     "http://localhost:3000",       # Desenvolvimento (alternativo)
     "http://127.0.0.1:5173",       # Desenvolvimento (IP local)
+    "http://localhost:5000",       # API local
 ]
 
 CORS(application, resources={
